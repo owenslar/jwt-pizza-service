52af7918bf54e899ec0ca760c36c1b9ff5e690f8
Owen Larson <owenslar@byu.edu>
2026-01-28 19:47:22 -0700

    added helper test functions

--- src/service.test.js
+++ src/service.test.js
@@ -1,14 +1,32 @@
 const request = require('supertest');
 const app = require('./service');
+const { Role, DB } = require('../database/database.js');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 // eslint-disable-next-line no-unused-vars
 let testUserAuthToken;
+// eslint-disable-next-line no-unused-vars
+let adminUser;
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+    user.name = randomName();
+    user.email = user.name + '@admin.com';
+
+    user = await DB.addUser(user);
+    return { ...user, password: 'toomanysecrets' };
+}
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
+
+  adminUser = await createAdminUser();
 });
 
 test('login', async () => {
